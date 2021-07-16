980ffd353dc2a09172c1b43d204f8637bc8f74bf
LockOriginalMods <81614998+LockOriginalMods@users.noreply.github.com>
2021-07-16 11:42:12 +0300

    Add files via upload

---
+++ src/main/java/com/LockOriginalMods/refinedforage/item/CuttersItem.java
@@ -0,0 +1,98 @@
+
+package com.LockOriginalMods.refinedforage.item;
+
+import com.LockOriginalMods.refinedforage.RefinedForage;
+import com.LockOriginalMods.refinedforage.RefinedforageModElements;
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.block.BlockState;
+
+
+import com.google.common.collect.Multimap;
+import com.google.common.collect.ImmutableMultimap;
+
+@RefinedforageModElements.ModElement.Tag
+public class CuttersItem extends RefinedforageModElements.ModElement {
+	@ObjectHolder("refinedforage:cutters")
+	public static final Item block = null;
+	public CuttersItem(RefinedforageModElements instance) {
+		super(instance, 79);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemToolCustom() {
+			@Override
+			public boolean hasCraftingRemainingItem() {
+				return true;
+			}
+
+			@Override
+			public ItemStack getContainerItem(ItemStack itemstack) {
+				ItemStack retval = new ItemStack(this);
+				retval.setDamageValue(itemstack.getDamageValue() + 1);
+				if (retval.getDamageValue() >= retval.getMaxDamage()) {
+					return ItemStack.EMPTY;
+				}
+				return retval;
+			}
+
+			@Override
+			public boolean isRepairable(ItemStack itemstack) {
+				return false;
+			}
+		}.setRegistryName("cutters"));
+	}
+	private static class ItemToolCustom extends Item {
+		protected ItemToolCustom() {
+			super(new Item.Properties().tab(RefinedForage.REFINEDFORAGE_GROUP).durability(100));
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack itemstack, BlockState blockstate) {
+			return 1;
+		}
+
+		@Override
+		public boolean mineBlock(ItemStack stack, World worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving) {
+			stack.hurtAndBreak(1, entityLiving, i -> i.broadcastBreakEvent(EquipmentSlotType.MAINHAND));
+			return true;
+		}
+
+		@Override
+		public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+			stack.hurtAndBreak(2, attacker, i -> i.broadcastBreakEvent(EquipmentSlotType.MAINHAND));
+			return true;
+		}
+
+		@Override
+		public int getEnchantmentValue() {
+			return 2;
+		}
+
+		@Override
+		public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlotType equipmentSlot) {
+			if (equipmentSlot == EquipmentSlotType.MAINHAND) {
+				ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+				builder.putAll(super.getDefaultAttributeModifiers(equipmentSlot));
+				builder.put(Attributes.ATTACK_DAMAGE,
+						new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Tool modifier", 2f, AttributeModifier.Operation.ADDITION));
+				builder.put(Attributes.ATTACK_SPEED,
+						new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Tool modifier", -3, AttributeModifier.Operation.ADDITION));
+				return builder.build();
+			}
+			return super.getDefaultAttributeModifiers(equipmentSlot);
+		}
+	}
+}
+
